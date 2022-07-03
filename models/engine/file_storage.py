1ace4179253cacd053571a68584e14721b6afae8
ZandileTshabalala20 <zandiletsh20@gmail.com>
2022-07-03 16:47:31 +0200

    commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,92 +1,55 @@
-#!/usr/bin/env python3
-"""Defines the FileStorage class."""
+#!usr/bin/python3
+""" class file_storage """
 import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from ..base_model import BaseModel
+from ..user import User
+from ..place import Place
+from ..state import State
+from ..city import City
+from ..amenity import Amenity
+from ..review import Review
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
+    """ Clase file Storage"""
 
     __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls=None):
-        """Return a dictionary of instantiated objects in __objects.
-        Return:
-            If a cls is specified, a dictionary of objects of that type.
-            Otherwise, the __objects dictionary.
-        """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            dictionary = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    dictionary[k] = v
-            return dictionary
-        return self.__objects
+    def __init__(self):
+        """ Init """
+        pass
+
+    def all(self):
+        """ return __objects """
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id."""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """ Create a new instance """
+        FileStorage.__objects["{}.{}".format(obj.__class__.__name__,
+                                             obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        dictionary = {}
-        for key in self.__objects:
-            dictionary[key] = self.__objects[key].to_dict(remove_password=False)
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(dictionary, f)
+        """ Save function """
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict.update({key: value.to_dict()})
+        json_file = json.dumps(new_dict)
+        with open(FileStorage.__file_path, "w") as my_file:
+            my_file.write(json_file)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """ Reload from JSON file """
+        my_dict = {"BaseModel": BaseModel, "User": User, "State": State,
+                   "City": City, "Amenity": Amenity, "Place": Place,
+                   "Review": Review}
+
+        json_file = ""
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
-        except FileNotFoundError:
+            with open(FileStorage.__file_path, "r") as my_file:
+                json_file = json.loads(my_file.read())
+                for key in json_file:
+                    FileStorage.__objects[key] = my_dict[json_file[key]['__clas\
+s__']](**json_file[key])
+        except:
             pass
-
-    def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
-
-    def close(self):
-        """Call the reload method."""
-        self.reload()
-
-    def get(self, cls, id):
-        """Returns a given instance from __objects.
-        Args:
-            cls (str): The class name of the instance to retrieve.
-            id  (str): The ID of the instance to retrieve.
-        """
-        if cls is None or id is None:
-            return None
-        key = '{}.{}'.format(cls, id)
-        return self.__objects.get(key, None)
-
-    def count(self, cls=None):
-        """Returns a count of all instances of the given class in __objects.
-        If no class is given, returns the total object count.
-        Args:
-            cls (str): The class type to count instances of.
-        """
-        if not cls:
-            return len(self.__objects)
-        return len([key for key in self.__objects if key.startswith(cls)])
